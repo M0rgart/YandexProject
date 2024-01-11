2ddc12da2205ef70720236245c5b48a0cd27141f
M0rgart <151552613+M0rgart@users.noreply.github.com>
2024-01-11 20:47:50 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
 import sys
 import pygame
+import random
 
 
 running = True
 FPS = 60
 WIDTH = 1366
 HEIGHT = 768
+
 
 pygame.init()
 pygame.key.set_repeat(200, 70)
@@ -15,23 +17,37 @@ clock = pygame.time.Clock()
 game_moment = "start"
 big_font = pygame.font.SysFont("comicsansms", 70)
 small_font = pygame.font.SysFont("kacstbook", 60)
-information = {"clas": False, "Fiz_dmg": False, "hp": False, "defence": False,
+information = {"class": False, "Fiz_dmg": False, "hp": False, "defence": False,
                "Mag_dmg": False, "LVL": False}
+room_num = 0
+location = 1
 
 def terminate():
     pygame.quit()
     sys.exit()
 
+
 #Стартовый экран, выбор персонажа или создание его
 def start_screen():
+    all_sprites = pygame.sprite.Group()
+    all_sprites.update()
+
+    class Tree(pygame.sprite.Sprite):
+        def __init__(self):
+            pygame.sprite.Sprite.__init__(self)
+            self.image = pygame.image.load('background1.png')
+            self.rect = self.image.get_rect()
+
+    all_sprites = pygame.sprite.Group()
+    bcg = Tree()
+    all_sprites.add(bcg)
+    all_sprites.draw(screen)
+
     game = big_font.render("Название игры",
                            True, (255, 165, 0))
     screen.blit(game,
                 (game.get_width() + 300,
                  game.get_height() - 50))
-
-    all_sprites = pygame.sprite.Group()
-    all_sprites.update()
 
     class NewGameSprite(pygame.sprite.Sprite):
         def __init__(self):
@@ -78,87 +94,219 @@ def start_screen():
     pygame.display.flip()
     clock.tick(FPS)
 
+
 #выбор класса персонажа
 def choose():
-    screen.fill((0, 0, 0))
+    all_sprites = pygame.sprite.Group()
+    all_sprites.update()
+
+    class Tree(pygame.sprite.Sprite):
+        def __init__(self):
+            pygame.sprite.Sprite.__init__(self)
+            self.image = pygame.image.load('background1.png')
+            self.rect = self.image.get_rect()
+
+    all_sprites = pygame.sprite.Group()
+    bcg = Tree()
+    all_sprites.add(bcg)
+    all_sprites.draw(screen)
+
     choose_class = big_font.render("выберете класс",
-                           True, (255, 165, 0))
+                           True, (255, 0, 0))
     screen.blit(choose_class,
                 ((screen.get_width() - choose_class.get_width()) // 2,
                  10))
 
-##    class knight(pygame.sprite.Sprite):
-##        def __init__(self):
-##            pygame.sprite.Sprite.__init__(self)
-##            self.image = pygame.Surface((50, 50))
-##            self.image = pygame.image.load("knight.png")
-##            self.image.set_colorkey((0, 0, 0))
-##            self.rect = self.image.get_rect()
-##            self.rect.center = (screen.get_width() // 3, 300)
-##
-##    all_sprites = pygame.sprite.Group()
-##    kng = knight()
-##    all_sprites.add(kng)
-##    all_sprites.draw(screen)
+    class knight(pygame.sprite.Sprite):
+        def __init__(self):
+            pygame.sprite.Sprite.__init__(self)
+            self.image = pygame.Surface((50, 50))
+            self.image = pygame.image.load("knight.png")
+            self.image.set_colorkey((0, 0, 0))
+            self.rect = self.image.get_rect()
+            self.rect.center = (screen.get_width() // 3, 300)
 
-##    class wizard(pygame.sprite.Sprite):
-##        def __init__(self):
-##            pygame.sprite.Sprite.__init__(self)
-##            self.image = pygame.Surface((50, 50))
-##            self.image = pygame.image.load("magic.png")
-##            self.image.set_colorkey((0, 0, 0))
-##            self.rect = self.image.get_rect()
-##            self.rect.center = (screen.get_width() // 3 * 2, 300)
-##
-##    all_sprites = pygame.sprite.Group()
-##    wiz = wizard()
-##    all_sprites.add(wiz)
-##    all_sprites.draw(screen)
+    all_sprites = pygame.sprite.Group()
+    kng = knight()
+    all_sprites.add(kng)
+    all_sprites.draw(screen)
+
+    class wizard(pygame.sprite.Sprite):
+        def __init__(self):
+            pygame.sprite.Sprite.__init__(self)
+            self.image = pygame.Surface((50, 50))
+            self.image = pygame.image.load("magic.png")
+            self.image.set_colorkey((0, 0, 0))
+            self.rect = self.image.get_rect()
+            self.rect.center = (screen.get_width() // 3 * 2, 300)
+
+    all_sprites = pygame.sprite.Group()
+    wiz = wizard()
+    all_sprites.add(wiz)
+    all_sprites.draw(screen)
 
     pygame.display.flip()
     clock.tick(FPS)
 
-#Окно сохдания персонажа: информация про классы
-def choose_class():
-    pass
 
 #Окно инвенторя, возможность сменить оружие, увидеть статистику персонажа
 #распределение очков характеристик
 def open_inventory():
-    pass
+    all_sprites = pygame.sprite.Group()
+    all_sprites.update()
+
+    class Tree(pygame.sprite.Sprite):
+        def __init__(self):
+            pygame.sprite.Sprite.__init__(self)
+            self.image = pygame.image.load('background1.png')
+            self.rect = self.image.get_rect()
+
+    all_sprites = pygame.sprite.Group()
+    bcg = Tree()
+    all_sprites.add(bcg)
+    all_sprites.draw(screen)
+
+    pygame.display.flip()
+    clock.tick(FPS)
+
 
 #Повышение уровня, +очки характеристик
 def lvl_up():
-    pass
+    all_sprites = pygame.sprite.Group()
+    all_sprites.update()
+
+    class Tree(pygame.sprite.Sprite):
+        def __init__(self):
+            pygame.sprite.Sprite.__init__(self)
+            self.image = pygame.image.load('background1.png')
+            self.rect = self.image.get_rect()
+
+    all_sprites = pygame.sprite.Group()
+    bcg = Tree()
+    all_sprites.add(bcg)
+    all_sprites.draw(screen)
+
+    pygame.display.flip()
+    clock.tick(FPS)
+
 
 #Комната с сундуком, вначале рандомим редкость, затем предмет этой редкости
 #Можно сделать псеводорандом на предметы класса (мечнику мечи пдают чаще луков)
 def chest():
-    pass
+    all_sprites = pygame.sprite.Group()
+    all_sprites.update()
+
+    class Tree(pygame.sprite.Sprite):
+        def __init__(self):
+            pygame.sprite.Sprite.__init__(self)
+            self.image = pygame.image.load(f'chest{location}.png')
+            self.rect = self.image.get_rect()
+
+    all_sprites = pygame.sprite.Group()
+    bcg = Tree()
+    all_sprites.add(bcg)
+    all_sprites.draw(screen)
+
+    pygame.display.flip()
+    clock.tick(FPS)
+
 
 #Комната с магазином, куить предметы или улучшить старые
 #сделать анимацию при выборе предметов
 def shop():
-    pass
+    all_sprites = pygame.sprite.Group()
+    all_sprites.update()
+
+    class Tree(pygame.sprite.Sprite):
+        def __init__(self):
+            pygame.sprite.Sprite.__init__(self)
+            self.image = pygame.image.load('background1.png')
+            self.rect = self.image.get_rect()
+
+    all_sprites = pygame.sprite.Group()
+    bcg = Tree()
+    all_sprites.add(bcg)
+    all_sprites.draw(screen)
+
+    pygame.display.flip()
+    clock.tick(FPS)
+
 
 #Бой, рандомный враг
 def battle():
-    pass
+    all_sprites = pygame.sprite.Group()
+    all_sprites.update()
+
+    mob = random.randint(1, 1)
+
+    class Enemy(pygame.sprite.Sprite):
+        def __init__(self):
+            pygame.sprite.Sprite.__init__(self)
+            self.image = pygame.image.load(f'enemy{mob}.png')
+            self.rect = self.image.get_rect()
+            self.rect.center = (900, 600)
+
+    class Tree(pygame.sprite.Sprite):
+        def __init__(self):
+            pygame.sprite.Sprite.__init__(self)
+            self.image = pygame.image.load('background1.png')
+            self.rect = self.image.get_rect()
+
+    all_sprites = pygame.sprite.Group()
+    bcg = Tree()
+    all_sprites.add(bcg)
+    all_sprites.draw(screen)
+
+    all_sprites = pygame.sprite.Group()
+    enemy = Enemy()
+    all_sprites.add(enemy)
+    all_sprites.draw(screen)
+
+    pygame.display.flip()
+    clock.tick(FPS)
+
 
 #Битв с боссом, пропаботать интелект получше чем в обычном бою
 def boss_battle():
-    pass
+    all_sprites = pygame.sprite.Group()
+    all_sprites.update()
+
+    class Tree(pygame.sprite.Sprite):
+        def __init__(self):
+            pygame.sprite.Sprite.__init__(self)
+            self.image = pygame.image.load('background1.png')
+            self.rect = (1000, 500)
+
+    all_sprites = pygame.sprite.Group()
+    bcg = Tree()
+    all_sprites.add(bcg)
+    all_sprites.draw(screen)
+
+    pygame.display.flip()
+    clock.tick(FPS)
+
 
 #генератор комнат, попытка перерботать этжи. комнаты более рандомные чем
 #в определенном порядке
 def room_generator():
-    pass
+    if room_num == 10:
+        return "boss_battle"
+    else:
+        rand = random.randint(1, 6)
+        if rand == 6:
+            return "shop"
+        elif rand == 5:
+            return "chest"
+        else:
+            return "battle"
+
 
 #Достижения
 def rewards():
     screen.fill((0, 0, 0))
     pygame.display.flip()
     clock.tick(FPS)
+
 
 while running:
     for event in pygame.event.get():
@@ -171,6 +319,22 @@ while running:
                     game_moment = "choose"
                 elif 1000 <= mouse[0] <= 1400 and 375 <= mouse[1] <= 425:
                     game_moment = "rewards"
+            elif game_moment == "choose":
+                if 0 <= mouse[0] <= (WIDTH // 3) and 200 <= mouse[1]:
+                    information['class'] = "knight"
+                    print(information)
+                    game_moment = room_generator()
+                elif ((WIDTH // 3) <= mouse[0] <= (WIDTH // 3) * 2
+                      and 200 <= mouse[1]):
+                    information['class'] = "hunter"
+                    print(information)
+                    game_moment = room_generator()
+                elif (WIDTH // 3) * 2 <= mouse[0] <= WIDTH and 200 <= mouse[1]:
+                    information['class'] = "mage"
+                    print(information)
+                    game_moment = room_generator()
+                else:
+                    pass
 
     if game_moment == "start":
         start_screen()
